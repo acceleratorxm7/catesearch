57131f8fd47ae97361f4a3e43e7ba81a34841ae4
infiniteburst <acceleratorxm7@gmail.com>
2022-09-21 09:02:49 +0900

    option edit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,69 @@
 import React, { useEffect, useState } from 'react';
 import { Row, Col, Container, Button } from 'react-bootstrap';
 import Select from 'react-select';
+import axios from "axios";
 
 function App() {
   const [options, setOptions] = useState({});
+  const [cate1, setCate1] = useState("");
+  const [cate2, setCate2] = useState("");
+  const [cate3, setCate3] = useState("");
+  const [cate4, setCate4] = useState("");
+  const [cate5, setCate5] = useState("");
   const [items, setItems] = useState({});
   const [logs, setLogs] = useState({});
 
+  const BASE_URL = "http://localhost:8080";
+
   useEffect(() => {
-    setOptions([
-      [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
-      ]
-    ])
+
+    const getOptions   = async () => {
+      let result = (await axios.get(`${BASE_URL}/options`)).data;
+      console.log(result);
+      let cate1 = result.map(el => { 
+        return { 
+          value: el.category1, 
+          label: el.category1 
+        }
+      });
+      let cate2 = result.map(el => { 
+        return { 
+          value: el.category2, 
+          label: el.category2 
+        }
+      });
+      let cate3 = result.map(el => { 
+        return { 
+          value: el.category3, 
+          label: el.category3 
+        }
+      });
+      let cate4 = result.map(el => { 
+        return { 
+          value: el.category4, 
+          label: el.category4 
+        }
+      });
+      let cate5 = result.map(el => { 
+        return { 
+          value: el.category5, 
+          label: el.category5 
+        }
+      });
+
+      let wrapper = [cate1, cate2, cate3, cate4, cate5];
+
+      setOptions(wrapper);
+    }
+
+    getOptions();
   }, []); 
   
   const loadResults = () => {
+    console.log(cate1, cate2, cate3, cate4, cate5);
     console.log("TEST");
   }
-
-  const loadOptions = () => {
-
-  }
-
+  
   return (
     <Container>
       <div style={{marginTop: "30px", marginBottom: "30px"}}>
@@ -32,12 +71,12 @@ function App() {
       </div>
 
       <Row>
-        <Col><Select options={options[0]} /></Col>
-        <Col><Select options={options[1]} /></Col>
-        <Col><Select options={options[2]} /></Col>
-        <Col><Select options={options[3]} /></Col>
-        <Col><Select options={options[4]} /></Col>
-        <Col><Button className="w-100" onClick={loadOptions()}>Run</Button></Col>
+        <Col><Select options={options[0]} onChange={choice => setCate1(choice)} /></Col>
+        <Col><Select options={options[1]} onChange={choice => setCate2(choice)} /></Col>
+        <Col><Select options={options[2]} onChange={choice => setCate3(choice)} /></Col>
+        <Col><Select options={options[3]} onChange={choice => setCate4(choice)} /></Col>
+        <Col><Select options={options[4]} onChange={choice => setCate5(choice)} /></Col>
+        <Col><Button className="w-100" onClick={loadResults()}>Run</Button></Col>
       </Row>
 
       {
